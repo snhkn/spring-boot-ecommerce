1f485e3132c1b173f536e82dbf8b765f9d98d565
snhkn <senihaakin@gmail.com>
2025-07-23 17:00:59 +0300

    Implement DTOs for get all categories

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -23,9 +24,9 @@ public class CategoryController {
 
     @GetMapping("/public/categories")
     //@RequestMapping(value="api/public/categories", method=RequestMethod.GET)
-    public ResponseEntity<List<Category>> getAllCategories(){
-        List<Category> categories = categoryService.getAllCategories();
-        return new ResponseEntity<>(categories, HttpStatus.OK);
+    public ResponseEntity<CategoryResponse> getAllCategories(){
+        CategoryResponse categoryResponse = categoryService.getAllCategories();
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
         //return ResponseEntity.ok(status);
         //return ResponseEntity.status(HttpStatus.OK).body();
     }
