b032c5fd2646dbd329c1d47c16246f7dec112ce7
snhkn <senihaakin@gmail.com>
2025-07-24 18:38:28 +0300

    Update Response Structure to include pagination details

--- src/main/java/com/ecommerce/project/payload/CategoryResponse.java
+++ src/main/java/com/ecommerce/project/payload/CategoryResponse.java
@@ -11,5 +11,11 @@ import java.util.List;
 @AllArgsConstructor
 public class CategoryResponse {
     private List<CategoryDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalPages;
+    private Boolean lastPage;
+
 
 }
