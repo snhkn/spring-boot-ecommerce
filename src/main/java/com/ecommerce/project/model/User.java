fc32b485507d134ea14bb212d6df43ba30d6cbf8
snhkn <senihaakin@gmail.com>
2025-08-06 18:59:55 +0300

    Add model classes: User, Role, Address

---
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -0,0 +1,38 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max =  50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max =  120)
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
