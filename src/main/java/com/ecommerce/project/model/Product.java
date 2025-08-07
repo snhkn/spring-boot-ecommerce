ba66663f8b8fe6f71e33bbe2e6ade905bdb959be
snhkn <senihaakin@gmail.com>
2025-08-07 14:18:14 +0300

    Add seller side of things

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -32,4 +32,9 @@ public class Product {
     @ManyToOne
     @JoinColumn(name="category_id")
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
+
 }
