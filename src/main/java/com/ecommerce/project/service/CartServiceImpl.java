5649df87491b5111e48cf2243fc62eb752a10747
snhkn <senihaakin@gmail.com>
2025-08-12 16:18:32 +0300

    Implement get user cart

--- src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -120,6 +120,26 @@ public class CartServiceImpl implements CartService{
         return cartDTOs;
     }
 
+    @Override
+    public CartDTO getCart(String emailId, Long cartId) {
+        Cart cart = cartRepository.findCartByEmailAndCartId(emailId, cartId);
+        if(cart==null){
+            throw new ResourceNotFoundException("Cart","carId", cartId);
+        }
+
+        CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+
+        cart.getCartItems().forEach(c->c.getProduct().setQuantity(c.getQuantity()));
+
+        List<ProductDTO> products = cart.getCartItems().stream()
+                .map(p-> modelMapper.map(p.getProduct(),ProductDTO.class ))
+                .toList();
+
+        cartDTO.setProducts(products);
+
+        return cartDTO;
+    }
+
     private Cart createCart(){
         Cart userCart = cartRepository.findCartByEmail(authUtil.loggedInEmail());
         if(userCart != null){
