6e640a139073ddeb1c2f4a9ab46def2bfe61221b
snhkn <senihaakin@gmail.com>
2025-09-08 16:50:31 +0300

    Implement getting addresses by user

--- src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -55,5 +55,13 @@ public class AddressServiceImpl implements AddressService {
         return modelMapper.map(address, AddressDTO.class);
     }
 
+    @Override
+    public List<AddressDTO> getUserAddresses(User user) {
+        List<Address> addresses = user.getAddresses();
+        return addresses.stream()
+                .map(address -> modelMapper.map(address, AddressDTO.class))
+                .collect(Collectors.toList());
+    }
+
 
 }
